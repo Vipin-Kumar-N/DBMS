14c897ff60ac1a92b359ac6f682a038519d0e29e
Vipin Kumar N <binduvipinkumar39@gmail.com>
2022-07-19 21:12:21 +0530

    Added Programs

--- PartBQuestion1.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-myclient=pymongo.MongoClient("mongodb://localhost:27017/")
-mydb=myclient["exam"]
-mycol=mydb["student"]
-
-updateId=mycol.update_many({"_id":4},{"$set" :{"Vaccination_status":"Both Vaccinated"}})
-print(updateId.modified_count," Updated Id:4 ")
-print()
--- PartBQuestion3.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-myclient=pymongo.MongoClient("mongodb://localhost:27017/")
-mydb=myclient["exam"]
-mycol=mydb["student"]
-
-deleteId=mycol.delete_many({"Lab_mark.Internal":{"$lte":30}})
-print(deleteId.deleted_count," Deleted Records with Internal Marks <=30 ")
-print()
